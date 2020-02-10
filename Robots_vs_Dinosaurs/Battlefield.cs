52ec341ecadc3fa9c67d102a54d6fd775d891d60
matt johannsen <majohann@hotmail.com>
2020-02-10 09:14:27 -0600

    Added method ChooseAttacker which uses switchcase statement

--- Robots_vs_Dinosaurs/Battlefield.cs
+++ Robots_vs_Dinosaurs/Battlefield.cs
@@ -27,10 +27,39 @@ namespace Robots_vs_Dinosaurs
             Console.ReadLine();
             theHerd.GetDinosaurs();
             theFleet.GetRobots();
+            Console.WriteLine(ChooseAttacker());
+            Console.ReadLine();
         }
-        public void GetHerd()
+        public string ChooseAttacker()
         {
-
+            string attackerChoice;
+            Console.WriteLine("Please choose your attacker (R1, R2, R3, D1, D2, or D3).");
+            attackerChoice = Console.ReadLine();
+            switch (attackerChoice)
+            {
+                case "R1":
+                    attackerChoice = "robot1";
+                    break;
+                case "R2":
+                    attackerChoice = "robot2";
+                    break;
+                case "R3":
+                    attackerChoice = "robot3";
+                    break;
+                case "D1":
+                    attackerChoice = "dinosaur1";
+                    break;
+                case "D2":
+                    attackerChoice = "dinosaur2";
+                    break;
+                case "D3":
+                    attackerChoice = "dinosaur3";
+                    break;
+                default:
+                    Console.WriteLine("This is not a valid selection.");
+                    break;
+            }
+            return attackerChoice;
         }
     }
 }
